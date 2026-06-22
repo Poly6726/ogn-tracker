80079b6e1417293644b132d7b0fa59347fb1e000
Jan Jakobsen <vtrman@freenet.de>
2026-06-22 21:31:41 +0200

    ESP32-c3 supermini with a RFM95 module seems to be working now.

--- src/esp32c3supermini-pins.h
+++ src/esp32c3supermini-pins.h
@@ -4,17 +4,17 @@
 #define GPS_UART UART_NUM_0  // UART for GPS
 #define GPS_PinTx      21 // Tx-Data
 #define GPS_PinRx      20 // Rx-Data
-#define GPS_PinPPS     0 // PPS
+//#define GPS_PinPPS     0 // PPS
 #define GPS_PinEna     3 // enable/wakeup
 
-// SX1262 RF chip
+// SX1262 or SX1276 RF chip
 #define Radio_PinRST    1 //
 #define Radio_PinSCK    4 // SCK
 #define Radio_PinMOSI   6 // MOSI
 #define Radio_PinMISO   5 // MISO
 #define Radio_PinCS     7 // CS
 #define Radio_PinIRQ1   3 // IRQ
-#define Radio_PinIRQ    Radio_PinIRQ1 // IRQ
+#define Radio_PinIRQ    Radio_PinIRQ1 // IRQ, DIO0 for SX1276
 #define Radio_PinBusy   2 // Busy: only for SX1262
 
 
@@ -23,8 +23,8 @@
 #define I2C_PinSDA    8 // SDA
 
 // Internal I2C for PMU
-#define PMU_I2C_PinSCL    1 // SCL
-#define PMU_I2C_PinSDA    2 // SDA
+//#define PMU_I2C_PinSCL    1 // SCL
+//#define PMU_I2C_PinSDA    2 // SDA
 
 // SD card and IMU       CONN   GPIO
 // SD_CS                  B-9    47
